8dc7bdd06273fa0ec54f1f9cfdbd2e075c2e4a75
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2023-03-05 14:32:07 +0100

    Create Menus.lua

---
+++ Components/Menus.lua
@@ -0,0 +1,11 @@
+Menus = {
+	BubbleTrouble = {
+		Name = "Bubble Trouble";
+		Description = "A menu specialized for a game called Bubble Gum TD";
+    GameDetect = 2428715111;
+		Search = "Bubble Gum Tower Defense";
+		Url = "https://raw.githubusercontent.com/Glowing-Red/Script-Hub/main/Bubble%20Gum%20Tower%20Defense.lua";
+	};
+}
+
+return Menus
