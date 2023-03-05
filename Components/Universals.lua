08e2b3903a0811fcaa6ada3827cf689a81937630
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2023-03-05 14:44:15 +0100

    Create Universals.lua

---
+++ Components/Universals.lua
@@ -0,0 +1,76 @@
+Universals = {
+	Hydroxide = {
+		Name = "Hydroxide";
+		Description = "Advanced Remote Spy tool that allows you to view remotes called";
+		Search = "Hydroxide";
+		Url = "https://raw.githubusercontent.com/Glowing-Red/Togas-Domain/main/Storage/Universals/Hydroxide.lua";
+	};
+	ServerBrowser = {
+		Name = "Server Browser";
+		Description = "A browser for server hop";
+		Search = "Server Browser";
+		Url = "https://raw.githubusercontent.com/Glowing-Red/Togas-Domain/main/Storage/Universals/Server%20Browser.lua";
+	};
+	CmdX = {
+		Name = "CMD-X";
+		Description = "Commandbar with client commands";
+		Search = "CMDX";
+		Url = "https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source";
+	};
+	R15AnimChange = {
+		Name = "Animation Changer [R15]";
+		Description = "Animation changer that only works on R15";
+		Search = "Animation Changer|R15";
+		Url = "https://raw.githubusercontent.com/ShiroSeby/Seby-s-Lair/main/R15%20Animation%20Changer";
+	};
+	AnimGrabV3 = {
+		Name = "Animation Grabber [V3]";
+		Description = "Logs the animations used on the client";
+		Search = "Animation Grabber [V3]";
+		Url = "https://raw.githubusercontent.com/BlastingStone/MyLuaStuff/master/animation_grabber_v3.lua";
+	};
+	InfiniteYield = {
+		Name = "Infinite Yield";
+		Description = "Commandbar with client commands";
+		Search = "Infinite Yield";
+		Url = "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source";	
+	};
+	CoordsFinder = {
+		Name = "Coords Finder";
+		Description = "Helps you make teleport functions";
+		Search = "Coords Finder";
+		Url = "https://grfbin.ml/scripts/coordsfinderv3.lua";	
+	};
+	DarkDexV3 = {
+		Name = "Dark Dex V3";
+		Description = "A version of the popular Dex explorer with patches specifically for Synapse X";
+		Search = "Dark Dex|Synapse X";
+		Url = "https://cdn.synapse.to/synapsedistro/hub/DarkDex.lua";	
+	};
+	DarkDexV4 = {
+		Name = "Dark Dex V4";
+		Description = "Popular dex explorer";
+		Search = "Dark Dex";
+		Url = "https://gist.githubusercontent.com/DinosaurXxX/b757fe011e7e600c0873f967fe427dc2/raw/ee5324771f017073fc30e640323ac2a9b3bfc550/dark%2520dex%2520v4";	
+	};
+	UnnamedESP = {
+		Name = "Unnamed ESP";
+		Description = "Popular ESP that supports Synapse X & Protosmasher";
+		Search = "Unnamed ESP|Synapse X|Protosmasher";
+		Url = "https://raw.githubusercontent.com/ic3w0lf22/Unnamed-ESP/master/UnnamedESP.lua";
+	};
+	RemoteSpy = {
+		Name = "Remote Spy";
+		Description = "Allows you to view RemoteEvents and RemoteFunctions called ";
+		Search = "Remote Spy|Synapse X";
+		Url = "https://cdn.synapse.to/synapsedistro/hub/RemoteSpy.lua";	
+	};
+	ScriptDump = {
+		Name = "Script Dumper";
+		Description = "Saves all Local & Module scripts";
+		Search = "Script Dumper|Synapse X";
+		Url = "https://cdn.synapse.to/synapsedistro/hub/ScriptDump.lua";	
+	};
+}
+
+return Universals
