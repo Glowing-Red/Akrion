cf3cbdc079d3b3c8dea21f69af207aeba77d2464
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2023-03-05 14:52:06 +0100

    Update Menu.lua

--- Components/Menu.lua
+++ Components/Menu.lua
@@ -4,6 +4,6 @@ return {
 		Description = "A menu specialized for a game called Bubble Gum TD";
 		GameDetect = 2428715111;
 		Search = "Bubble Gum Tower Defense";
-		Url = "https://raw.githubusercontent.com/Glowing-Red/Script-Hub/main/Bubble%20Gum%20Tower%20Defense.lua";
+		Url = "https://raw.githubusercontent.com/Glowing-Red/Togas-Domain/main/Storage/Menus/Bubble%20Gum%20Tower%20Defense.lua";
 	};
 }
