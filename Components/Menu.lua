88b7aca406be29a429a6bee527942e5ff661aee1
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2023-03-05 15:38:21 +0100

    Update Menu.lua

--- Components/Menu.lua
+++ Components/Menu.lua
@@ -3,14 +3,14 @@ return {
 		Name = "Bubble Trouble";
 		Description = "Exclusive menu specialized for a game called Bubble Gum Tower Defense";
 		GameDetect = 2428715111;
-		Search = "Bubble Gum Tower Defense";
+		Search = "Bubble Gum Tower Defense|Exclusive";
 		Url = "https://raw.githubusercontent.com/Glowing-Red/Akrion/main/Exclusive/Bubble%20Gum%20TD.lua";
 	};
 	TowerBlitz = {
 		Name = "Tower Blitz";
 		Description = "Exclusive marco menu made for Tower Blitz";
 		GameDetect = 1582293268;
-		Search = "Tower Blitz";
+		Search = "Tower Blitz|Exclusive";
 		Url = "https://raw.githubusercontent.com/Glowing-Red/Akrion/main/Exclusive/Tower%20Blitz.lua";
 	};
 }
