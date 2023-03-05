a555b698ef5f0bcab64d5a2ad94ba63674238e3d
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2023-03-05 14:24:36 +0100

    Create Hubs.lua

---
+++ Components/Hubs.lua
@@ -0,0 +1,16 @@
+Hubs = {
+	EzHub = {
+		Name = "EZHub";
+    Description = "A menu with a large variety of different scripts stored within it!";
+		Search = "EzHub";
+		Url = "https://raw.githubusercontent.com/debug420/Ez-Industries-Launcher-Data/master/Launcher.lua";
+	},
+	MrJack = {
+		Name = "Mr Jack";
+    Description = "";
+		Search = "MrJack||Mr Jack";
+		Url = "https://thedragonslayer2.github.io/AutoExec.html";
+	},
+}
+
+return Hubs
