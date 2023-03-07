a6aa50db781c3da0605dd74a19b11108fe3c97ae
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2023-03-07 11:53:19 +0100

    Update Menu.lua

--- Components/Menu.lua
+++ Components/Menu.lua
@@ -22,7 +22,7 @@ return {
 	};
 	Intruders = {
 		Name = "Anime Dimensions Intruders";
-		Description = "Menu that was specialized for Anime Dimensions Simulator and released by ''The Intruders''";
+		Description = "Menu that was specialized for Anime Dimensions Simulator and released by 'The Intruders'";
 		GameDetect = 2655311011;
 		Search = "Anime Dimensions Simulator";
 		Url = "https://raw.githubusercontent.com/lifaiossama/errors/main/Intruders.html";
